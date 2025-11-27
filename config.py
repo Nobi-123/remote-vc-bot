85893e1dac2084cb0f44584895db175d1184f9e6
Nobi-123 <nobita.infringement.report@gmail.com>
2025-11-27 16:32:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
+import os
 
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OWNER_ID = int(os.getenv("OWNER_ID"))
+PREFIX = os.getenv("PREFIX", "/", ".", "!")
+
+SESSIONS_DIR = "sessions"
+RECORDINGS_DIR = "recordings"
+TEMP_DIR = "temp"
+
+os.makedirs(SESSIONS_DIR, exist_ok=True)
+os.makedirs(RECORDINGS_DIR, exist_ok=True)
+os.makedirs(TEMP_DIR, exist_ok=True)
+
+sudo_users = {OWNER_ID}
