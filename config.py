80b4a8e86d653a009df1e597bda9e9af676bb9df
Nobi-123 <nobita.infringement.report@gmail.com>
2025-11-27 18:14:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-OWNER_ID = int(os.getenv("OWNER_ID"))
+API_ID = int(os.getenv("API_ID", "22657083"))
+API_HASH = os.getenv("API_HASH", "d6186691704bd901bdab275ceaab88f3")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8314045999:AAFk45sqRkbIUXdK3F9t0wsKXTvub6l8zfw")
+OWNER_ID = int(os.getenv("OWNER_ID", "8449801101"))
 PREFIX = os.getenv("PREFIX", "/")
 
 SESSIONS_DIR = "sessions"
