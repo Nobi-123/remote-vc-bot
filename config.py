ec410f6e23011bc60f8f5fe2c03240a9bc4ac9c8
Nobi-123 <nobita.infringement.report@gmail.com>
2025-11-27 17:49:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID"))
-PREFIX = os.getenv("PREFIX", "/", ".", "!")
+PREFIX = os.getenv("PREFIX", "/")
 
 SESSIONS_DIR = "sessions"
 RECORDINGS_DIR = "recordings"
